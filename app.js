84ecd6ded6e514b90a8082a9a434f311e1f6d922
okafor ebuka <122581334+grunde1234@users.noreply.github.com>
2024-01-20 14:04:36 +0100

    Update app.js

---
+++ app.js
@@ -0,0 +1,46 @@
+//Loading animation
+window.addEventListener("load", () => {const load = document.querySelector(".load");
+
+load.classList.add("load-hidden");
+
+load.addEventListener("transitioned", () =>{ document.body.removeChild("load");
+})
+})
+
+const fileInput = document.querySelector('input'),
+downloadBtn = document.querySelector('button');
+
+
+
+
+const down = downloadBtn.addEventListener('click',e=>{
+    e.preventDefault();// Stops default settings
+    downloadBtn.innerText = "Downloading file...";
+    fetchFile(fileInput.value);
+    console.log(fileInput.value);
+
+});
+
+function fetchFile(url){
+    fetch(url).then(res => res.blob()).then(file => {
+        let fetchURL = URL.createObjectURL(file);//Creates a url of the file
+        let tag = document.createElement('a');
+        tag.href = fetchURL;
+         tag.download = url.replace(/^.*[\\\/]/, '');
+         document.body.appendChild(tag);
+         console.log(fetchURL);
+         tag.click();
+         tag.remove();
+         URL.revokeObjectURL(fetchURL);
+            downloadBtn.innerText = "Downloaded &#9989";
+        
+    }).catch(()=>{
+        alert('Failed to download');
+    })
+}
+
+
+
+downloadBtn.addEventListener('keydown',()=>{
+    down(event);
+})//On clicking enter the file starts to download
